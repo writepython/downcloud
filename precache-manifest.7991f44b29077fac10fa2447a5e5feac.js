953d375b658066c1494f59cfd9c3b1973e3fd9ec
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-22 08:11:53 +0000

    Deploy downcloud.ruud.ninja to github.com/tomasruud/downcloud.git:gh-pages

---
+++ precache-manifest.7991f44b29077fac10fa2447a5e5feac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2e1d8df00afd5b6fe46768a052612ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60e1d03906814c80f6ed",
+    "url": "/static/js/2.5a3b828b.chunk.js"
+  },
+  {
+    "revision": "17100c8c3f9fc3b14ffb7f5a0710499d",
+    "url": "/static/js/2.5a3b828b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93d9b84870d87301f7d3",
+    "url": "/static/js/main.b4ea2918.chunk.js"
+  },
+  {
+    "revision": "1b04f44ea7dc9d4dd3b2",
+    "url": "/static/js/runtime-main.1ae313b1.js"
+  }
+]);
